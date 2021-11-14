bc57aa0e9ebc336e587ad5e596859a6dc6232d50
S <85238140+samratcode@users.noreply.github.com>
2021-11-14 16:13:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,22 +7,22 @@ if os.path.exists("local.env"):
 
 load_dotenv()
 admins = {}
-SESSION_NAME = getenv("SESSION_NAME", "session")
-BOT_TOKEN = getenv("BOT_TOKEN")
-BOT_NAME = getenv("BOT_NAME", "Video Stream")
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
-DATABASE_URL = os.getenv("DATABASE_URL")
-OWNER_NAME = getenv("OWNER_NAME", "dlwrml")
-ALIVE_NAME = getenv("ALIVE_NAME", "Levina")
-BOT_USERNAME = getenv("BOT_USERNAME", "veezvideobot")
-ASSISTANT_NAME = getenv("ASSISTANT_NAME", "cleo_invida")
-GROUP_SUPPORT = getenv("GROUP_SUPPORT", "VeezSupportGroup")
-UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "levinachannel")
-SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
-COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
-ALIVE_IMG = getenv("ALIVE_IMG", "https://telegra.ph/file/c83b000f004f01897fe18.png")
-DURATION_LIMIT = int(getenv("DURATION_LIMIT", "60"))
+SESSION_NAME = "AQAP3fV9d7EG9IHVTHrfPFoRXSFtqPHcPHOzAUvw1kO0EDreOAOgptK1oGLDjToNK3H4R7mF_9f3aMbkgyF0Dn6fAgI0HukrDXLFbwwDENWI9nPFYH3SmJiLDDhlvAIu23JVw4-m1Ri3f8G77-GgbsWM4q7Cst1UKJpk_slyZvWGq2lxfZ6rcqhN1cFJ_CLIrcs0dOLUbbeKFy_ie96SlsaZKcDzi4DIYA6lYz6-DY4rtRQ33wQjxye7L4Au7VIF7Nv5OpqG6U9ELe3upND_l_6FYZSMGlqwlz65q2077luWgWx8d6R3R10vLfZPWLvqUoKCmEsfuJsqfpwlZvJh-0XMdcSqmwA"
+BOT_TOKEN = "1931163050:AAFaNETwdYhxwU1DlHZEMW3tsfMhdB9z8kQ"
+BOT_NAME = "Sayu"
+API_ID = 8896402
+API_HASH = "de2f306c240b3036cac3866e738fad3f"
+DATABASE_URL = "mongodb+srv://fridayub:7352007414@cluster0.garnp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+OWNER_NAME = "TheSexyKid"
+ALIVE_NAME = "SayuMusic"
+BOT_USERNAME = "SayuVc"
+ASSISTANT_NAME = "SayuVc"
+GROUP_SUPPORT = "RukaSupport"
+UPDATES_CHANNEL = "Rukaupdates"
+SUDO_USERS = "1634155867"
+COMMAND_PREFIXES = "/"
+ALIVE_IMG = "https://telegra.ph/file/16fd73ce91fb4e569db9e.jpg")
+DURATION_LIMIT = "60"
 IMG_1 = getenv("IMG_1", "https://telegra.ph/file/d6f92c979ad96b2031cba.png")
 IMG_2 = getenv("IMG_2", "https://telegra.ph/file/6213d2673486beca02967.png")
 IMG_3 = getenv("IMG_3", "https://telegra.ph/file/f02efde766160d3ff52d6.png")
